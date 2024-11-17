ba7f8043ddf2677e3abf94b7aeace9d91e10acd6
RodrigoSilva04 <2231653@my.ipleiria.pt>
2024-11-17 22:18:19 +0000

    Alterações no layout

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -83,7 +83,7 @@ AppAsset::register($this);
 
 
 <main role="main" class="flex-shrink-0">
-    <div class="container">
+    <div class="container-fluid">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
