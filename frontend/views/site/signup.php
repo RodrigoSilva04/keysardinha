333c34ccf174f82213964865183237374a1c3a0b
Fábio Igreja <2232332@my.ipleiria.pt>
2024-11-18 15:38:46 +0000

    Ajuste de layout e design

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -10,12 +10,12 @@ use yii\bootstrap5\ActiveForm;
 $this->title = 'Registar';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-signup">
+<div class="site-signup d-flex flex-column align-items-center justify-content-center" style="min-height: 100vh;">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Por favor preencha os com os seus dados</p>
 
-    <div class="row">
+    <div class="row justify-content-center w-100">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <?= $form->field($model, 'password')->passwordInput(['class' => 'form-control']) ?>
 
-            <div class="form-group">
+            <div class="form-group text-center">
                 <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
             </div>
 
