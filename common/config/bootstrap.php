f76f833eb11b1d4adfcc253afbb023d18d335ec4
2231644 <2231644@my.ipleiria.pt>
2025-01-08 20:06:13 +0000

    Merge

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,6 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+
+Yii::setAlias('@web', 'http://projeto/frontend/web/site/index'); // Substitua pela URL desejada
+Yii::setAlias('@webroot', dirname(dirname(__DIR__)) . '/frontend/web/site/index'); // Caminho físico correspondente
